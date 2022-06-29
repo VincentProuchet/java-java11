11de21180979d3d5596f34921281ba0440d9ca1d
Formation <Formation@DESKTOP-2MQ0DVS>
2022-06-29 17:42:06 +0200

    Corrigé Ex 3

--- src/test/java/java8/ex03/Stream_03_Test.java
+++ src/test/java/java8/ex03/Stream_03_Test.java
@@ -33,50 +33,44 @@ public class Stream_03_Test {
 	public void test_max() throws Exception {
 
 		List<Order> orders = new Data().getOrders();
-		
-		Comparator<Order> comparerLesPrixl =new Comparator<Order>(){
 
-			@Override
-			public int compare(Order o1, Order o2) {
-				int result = 0;
-				if(o1.getPrice()>o2.getPrice()) {
-					result = 1;
-				}else if (o1.getPrice()<o2.getPrice()) {
-					result =-1;
-				}
-				// TODO Auto-generated method stub
-				return result;
+		Comparator<Order> comparerLesPrixl = (Order o1, Order o2) -> {
+			int result = 0;
+			if (o1.getPrice() > o2.getPrice()) {
+				result = 1;
+			} else if (o1.getPrice() < o2.getPrice()) {
+				result = -1;
 			}
-			
-		
+			// TODO Auto-generated method stub
+			return result;
 		};
 
 		// TODO Retrouver la commande avec le prix le plus élevé
-		Optional<Order> result = orders.stream()
-						.max(comparerLesPrixl) ;
-
+		Optional<Order> result = orders.stream().max(comparerLesPrixl);
+		// autre methode en utilisant un comparator automatique le comparing
+		Optional<Order> result2 = orders.stream().max(Comparator.comparing(o -> o.getPrice()));
+		// la methode par reference de méthode dans le comparing
+		Optional<Order> result3 = orders.stream().max(Comparator.comparing(Order::getPrice));
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.get().getPrice(), is(2200.0));
 	}
 
 	@Test
 	public void test_min() throws Exception {
-		
-		
-		
+
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Retrouver la commande avec le prix le plus élevé
-		Optional<Order> result = orders.stream().min((Order o1, Order o2)-> {
-				int resultat = 0;
-				if(o1.getPrice()>o2.getPrice()) {
-					resultat = 1;
-				}else if (o1.getPrice()<o2.getPrice()) {
-					resultat =-1;
-				}
-				// TODO Auto-generated method stub
-				return resultat;
-			});
+		Optional<Order> result = orders.stream().min((Order o1, Order o2) -> {
+			int resultat = 0;
+			if (o1.getPrice() > o2.getPrice()) {
+				resultat = 1;
+			} else if (o1.getPrice() < o2.getPrice()) {
+				resultat = -1;
+			}
+			// TODO Auto-generated method stub
+			return resultat;
+		});
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.get().getPrice(), is(1000.0));
@@ -84,17 +78,15 @@ public class Stream_03_Test {
 
 	@Test
 	public void test_map_collect_joining() throws Exception {
-		
-	
 
 		List<Customer> customers = new Data().getCustomers();
 
 		// TODO construire une chaîne contenant les prénoms des clients triés et séparés
 		// par le caractère "|"
-		String result = customers.stream().map(c->c.getFirstname())
-				.sorted()
-				.spliterator(n ->{ return n+"|";})
-				;
+		// c'était tellement simple que j'aurais dut pouvoir le trouver tout seul
+		String result = customers.stream().map(c -> c.getFirstname()).sorted().collect(Collectors.joining("|"))
+
+		;
 
 		assertThat(result, is("Alexandra|Cyril|Johnny|Marion|Sophie"));
 	}
@@ -105,7 +97,8 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Extraire la liste des pizzas de toutes les commandes
-		List<Pizza> result = null;
+		// non trouvé seul
+		List<Pizza> result = orders.stream().flatMap(o->o.getPizzas().stream()).toList();
 
 		assertThat(result.size(), is(9));
 	}
@@ -116,7 +109,8 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Extraire la liste des différentes pizzas de toutes les commandes
-		List<Pizza> result = null;
+		// non trouvé seul
+		List<Pizza> result = orders.stream().flatMap(o->o.getPizzas().stream()).distinct().toList();
 
 		assertThat(result.size(), is(4));
 	}
@@ -127,7 +121,8 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO construire une Map <Client, Commandes effectuées par le client
-		Map<Customer, List<Order>> result = null;
+		// non trouvé seul
+		Map<Customer, List<Order>> result = orders.stream().collect(Collectors.groupingBy(o->o.getCustomer()));
 
 		assertThat(result.size(), is(2));
 		assertThat(result.get(new Customer(1)), hasSize(4));
@@ -141,7 +136,7 @@ public class Stream_03_Test {
 		// TODO Séparer la liste des pizzas en 2 ensembles :
 		// TODO true -> les pizzas dont le nom commence par "L"
 		// TODO false -> les autres
-		Map<Boolean, List<Pizza>> result = null;
+		Map<Boolean, List<Pizza>> result = pizzas.stream().collect(Collectors.partitioningBy(p->p.getName().startsWith("L")));
 
 		assertThat(result.get(true), hasSize(6));
 		assertThat(result.get(false), hasSize(2));
